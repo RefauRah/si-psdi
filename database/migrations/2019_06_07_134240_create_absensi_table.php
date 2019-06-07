4ce5b46c62d3e7458dcac7d84580d75500599227
broGasai <brogalangsatria@gmail.com>
2019-06-07 22:51:48 +0700

    add database

---
+++ database/migrations/2019_06_07_134240_create_absensi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAbsensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absensi', function (Blueprint $table) {
+            $table->bigIncrements('id_absen');
+            $table->integer('id_siswa');
+            $table->char('status_hadir',100);
+            $table->integer('pertemuanke');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absensi');
+    }
+}
---
+++ database/migrations/2019_06_07_134824_create_siswa_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->bigIncrements('id_siswa');
+            $table->integer('nim');
+            $table->char('nama',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
