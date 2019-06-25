ea4477f8725849d55f00138844c340130e43fca9
broGasai <brogalangsatria@gmail.com>
2019-06-25 23:24:08 +0700

    tambah bangsat

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\User;
+use DB;
 class DashboardController extends Controller
 {
     public function index()
@@ -24,13 +25,12 @@ class DashboardController extends Controller
         return view('admin/admin/create');
     }
 
-        public function store(Request $request)
+    public function store(Request $request)
     {
 
         date_default_timezone_set("Asia/Bangkok");
         User::create([
             'name'=>$request->name,
-            'username' =>$request->username,
             'email'=>$request->email,
             'password'=>bcrypt ($request->password),
             'user_type'=>$request->user_type
@@ -39,7 +39,38 @@ class DashboardController extends Controller
         \Session::flash('flash_message','successfully saved.');
         return redirect('/admin');
 
-        \Session::flash('flash_message','successfully saved.');
+    }
+
+    public function hapus($user)
+    {
+        DB::table('users')->where('id',$user)->delete();
+        
+    // alihkan halaman ke halaman guru
+        return redirect('/admin');
 
     }
+
+    // method untuk edit data pegawai
+    public function edit($user)
+    {
+        // mengambil data pegawai berdasarkan id yang dipilih
+        DB::table('users')->where('id',$user)->get();
+        // passing data pegawai yang didapat ke view edit.blade.php
+        return view('/admin/admin/edit',['users' => $user]);
+
+    }
+
+    public function update(Request $request)
+    {
+    // update data pegawai
+    date_default_timezone_set("Asia/Bangkok");
+    DB::table('users')->where('id',$request->id)->update([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>bcrypt ($request->password),
+        'user_type'=>$request->user_type
+    ]);
+    // alihkan halaman ke halaman pegawai
+    return redirect('/admin');
+}
 }
